fa9ceedc376a56e5a14c273d47414c14f765101d
Jesper <jejo7777@gmail.com>
2016-08-30 12:33:59 +0200

    NameTyper v.1 First draft

---
+++ NamePrinter/NamePrinter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamePrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your first name?: ");
+            string firstName = Console.ReadLine();
+            Console.Write("\nWhat's your last name?: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("\nHej " + firstName + " " + lastName);
+        }
+    }
+}
