856aa992956b4271ea2afe9968842189f2d8419a
Jesper <jejo7777@gmail.com>
2016-08-30 12:41:56 +0200

    NameTyper v.1.2 - Removed the first name requirement

--- NamePrinter/NamePrinter/Program.cs
+++ NamePrinter/NamePrinter/Program.cs
@@ -10,11 +10,9 @@ namespace NamePrinter
     {
         static void Main(string[] args)
         {
-            Console.Write("What's your first name?: ");
-            string firstName = Console.ReadLine();
-            Console.Write("\nWhat's your last name?: ");
+            Console.Write("\nHello, what's your last name?: ");
             string lastName = Console.ReadLine();
-            Console.WriteLine("\nHej " + firstName + " " + lastName);
+            Console.WriteLine("\nHej " + lastName);
         }
     }
 }
