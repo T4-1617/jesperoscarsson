355d4ef45e61ab96cec503416faf80561c71c8a9
Jesper <jejo7777@gmail.com>
2016-08-31 12:42:50 +0200

    First version

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double numberOne = double.Parse(Console.ReadLine()); //Retrieves a string from Console and converts it into double for saving
+
+            Console.Write("\nEnter the Second number: ");
+            double numberTwo = double.Parse(Console.ReadLine()); //Retrieves a string from Console and converts it into double for saving
+
+            Console.Write("\n\nEnter your operation: + | - | / | * : ");
+            string operation = Console.ReadLine(); //Saves the operator in a string
+
+            double answer = 0; //Initialize a variable that will hold the answer later
+
+            switch (operation) //Matches the selected operator and calculates answer
+            {
+                case "+":
+                    answer = numberOne + numberTwo;
+                    break;
+                case "-":
+                    answer = numberOne - numberTwo;
+                    break;
+                case "/":
+                    answer = numberOne / numberTwo;
+                    break;
+                case "*":
+                    answer = numberOne * numberTwo;
+                    break;
+                default: //If a invalid value is named this appears and the answer stays at 0
+                    Console.Write("\n\nError,invalid entry!");
+                    break;
+            }
+            Console.WriteLine("\nAnswer: " + answer);
+        }
+    }
+}
