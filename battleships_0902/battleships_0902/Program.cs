2f2c9dc5b3dacc14cc217f7eaa435e1cd98f2380
Jesper <jejo7777@gmail.com>
2016-09-02 13:43:43 +0200

    Added random boat spawns, very messy code atm

--- battleships_0902/battleships_0902/Program.cs
+++ battleships_0902/battleships_0902/Program.cs
@@ -17,10 +17,71 @@ namespace battleships_0902
 
         static void Main(string[] args)
         {
-            bool[,] gridPLayer = new bool[7,5];
-            bool[,] gridAI = new bool[7, 5];
 
-            int boatSpawn = randomNumber(3, 5);
+            int boatSpawn = randomNumber(4, 6);
+            
+            //BROKEN DO NOT USE!!!
+            int[,] boat = new int[boatSpawn, boatSpawn];
+            int[] boatX = new int[boatSpawn];
+            int[] boatY = new int[boatSpawn];
+            Console.WriteLine("X:\tY:");
+            for (int i = 0; i < boatSpawn; i++)
+            {
+                boatX[i] = randomNumber(0, 7);
+                System.Threading.Thread.Sleep(1000);
+                boatY[i] = randomNumber(0, 5);
+                Console.Write(boatX[i] + "\t");
+                Console.WriteLine(boatY[i]);
+            }
+
+            bool[,] gridPlayer = new bool[7, 5];
+
+            //Fill grid with empty space
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    gridPlayer[x, y] = false;
+                }
+            }
+
+            //Placera ut några båtar på spelplanen
+            for (int j = 0; j < boatSpawn; j++)
+            {
+                gridPlayer[boatX[j], boatY[j]] = true;
+            }
+
+            //Vi testar att skriva ut hela spelplanen
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    string p;
+                    if (gridPlayer[x, y] == true)
+                    {
+                        p = " X ";
+                    }
+                    else
+                    {
+                        p = " O ";
+                    }
+                    //Console.Write(GameMap[x, y]); 
+                    Console.Write(p);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
