23dd4c1436e6ae276695326e4fa596f5feffc2e3
Jesper <jejo7777@gmail.com>
2016-09-12 13:29:05 +0200

    Added class for customer info and button to add info to object.

---
+++ CRM 0912/CRM 0912/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_0912
+{
+    class Customers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Active { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
--- CRM 0912/CRM 0912/Form1.cs
+++ CRM 0912/CRM 0912/Form1.cs
@@ -17,10 +17,36 @@ namespace CRM_0912
             InitializeComponent();
         }
 
+        //Function for generating unique ID number in the 100-200 range (did not use random since it wasn't specified
+        public int getIDNumber()
+        {
+            IDcounter++;
+            IDNumber = IDcounter;
+
+            return IDNumber;
+        }
+
+        //Array for storing customer values
+        System.Collections.ArrayList CustomerList = new System.Collections.ArrayList();
+        //Variables
+        static int IDcounter = 99;
+        static int IDNumber = 0;
+
         //Executes when button is pressed
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            if (IDcounter <= 200)
+            {
+                //Create new object from class Customers
+                Customers customerInfo = new Customers();
+                //Retrieve first & second name
+                customerInfo.FirstName = txtBoxFName.Text;
+                customerInfo.LastName = txtBoxLName.Text;
+                customerInfo.Active = true;
+                customerInfo.CustomerID = getIDNumber();
+                //Add new info to list
+                CustomerList.Add(customerInfo);
+            }
         }
     }
 }
