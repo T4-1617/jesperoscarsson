5705a65a612ad60697b327260a9acf1bb955ce92
Jesper <jejo7777@gmail.com>
2016-09-14 13:19:31 +0200

    First commit, built GUI and base of class "cars.cs"

---
+++ 0914-CarProject-0.1/0914-CarProject-0.1/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0914_CarProject_0._1
+{
+    class Car
+    {
+        public int regNumb { get; set; }
+        public string propelant { get; set; }
+        public string transmission { get; set; }
+        public bool available { get; set; }
+        public int seats { get; set; }
+        public int doors { get; set; }
+        public string baggageSpace { get; set; }
+    }
+}
