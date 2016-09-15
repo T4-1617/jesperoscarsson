bdb455333cec0c3f046f475413f444523ac5e0e7
Jesper <jejo7777@gmail.com>
2016-09-15 19:44:50 +0200

    Initial commit, began building GUI (panel needs more work)

---
+++ ACNECARS/ACNECARS/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ACNECARS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCarsAvailable = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnShowCars = new System.Windows.Forms.Button();
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.btnReturnCar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ACNE CARS v.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "We have";
+            // 
+            // lblCarsAvailable
+            // 
+            this.lblCarsAvailable.AutoSize = true;
+            this.lblCarsAvailable.Location = new System.Drawing.Point(69, 34);
+            this.lblCarsAvailable.Name = "lblCarsAvailable";
+            this.lblCarsAvailable.Size = new System.Drawing.Size(149, 13);
+            this.lblCarsAvailable.TabIndex = 2;
+            this.lblCarsAvailable.Text = "amountCarsForHire Goes here";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(224, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "cars available";
+            // 
+            // btnShowCars
+            // 
+            this.btnShowCars.Location = new System.Drawing.Point(3, 65);
+            this.btnShowCars.Name = "btnShowCars";
+            this.btnShowCars.Size = new System.Drawing.Size(75, 23);
+            this.btnShowCars.TabIndex = 4;
+            this.btnShowCars.Text = "Show cars";
+            this.btnShowCars.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Location = new System.Drawing.Point(84, 65);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCar.TabIndex = 5;
+            this.btnAddCar.Text = "Add car";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            // 
+            // btnReturnCar
+            // 
+            this.btnReturnCar.Location = new System.Drawing.Point(165, 65);
+            this.btnReturnCar.Name = "btnReturnCar";
+            this.btnReturnCar.Size = new System.Drawing.Size(75, 23);
+            this.btnReturnCar.TabIndex = 6;
+            this.btnReturnCar.Text = "Return car";
+            this.btnReturnCar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(12, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(457, 347);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 453);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnReturnCar);
+            this.Controls.Add(this.btnAddCar);
+            this.Controls.Add(this.btnShowCars);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCarsAvailable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCarsAvailable;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnShowCars;
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Button btnReturnCar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
