95db7de042bf4a82e3ee0867ae3119f7a50af140
Jesper <jejo7777@gmail.com>
2016-09-27 13:45:32 +0200

    09/27 - Initial Commit, Created html files, css file and controller.

---
+++ 160927_PersonalPage/160927_PersonalPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_160927_PersonalPage.MvcApplication" Language="C#" %>
---
+++ testDemo/testDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testDemo.Startup))]
+namespace testDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
