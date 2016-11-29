fb8283465a9dd67b74927cdfa7ab5aaf7946bdae
Jesper <jejo7777@gmail.com>
2016-11-29 11:33:44 +0100

    Added projects view

--- MyPage/MyPage/Controllers/HomeController.cs
+++ MyPage/MyPage/Controllers/HomeController.cs
@@ -8,8 +8,14 @@ namespace MyPage.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Projects()
         {
             return View();
         }
